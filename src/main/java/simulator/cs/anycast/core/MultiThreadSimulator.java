081f5f1ec8982d2df2b5b91b63cb9ed66047dee6
Carlos Natalino Da Silva <6130746+carlosnatalino@users.noreply.github.com>
2020-02-13 11:41:17 +0100

    Initial commit

---
+++ src/main/java/simulator/cs/anycast/core/MultiThreadSimulator.java
@@ -0,0 +1,20 @@
+package simulator.cs.anycast.core;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ *
+ * @author carda
+ */
+public class MultiThreadSimulator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Testing!");
+    }
+    
+}
